5001793642a853f83bdd78754decb5fe62c5c3d3
Massimo Rossello <massimo.rossello@artistii.com>
2014-01-24 14:45:27 +0100

    Solved a severe infinite recursion due to wrong, maybe multiple, aliasing

--- init.rb
+++ init.rb
@@ -9,9 +9,9 @@ Rails.logger.info 'Starting Watcher Groups plugin for Redmine'
 
 Rails.configuration.to_prepare do
 
-    if Issue.method_defined?(:notified_watchers)
-       Issue.send(:include, WatcherGroupsWatcherHelperPatch)
-    end
+  unless Issue.included_modules.include?(WatcherGroupsWatcherHelperPatch)
+    Issue.send(:include, WatcherGroupsWatcherHelperPatch)
+  end
 
 end
 
@@ -19,7 +19,7 @@ Redmine::Plugin.register :redmine_watcher_groups do
   name 'Redmine Watcher Groups plugin'
   author 'Kamen Ferdinandov, Massimo Rossello'
   description 'This is a plugin for Redmine to add watcher groups functionality'
-  version '0.0.2'
+  version '0.0.3'
   url 'http://github.com/maxrossello/redmine_watcher_groups'
   author_url 'http://github.com/maxrossello'
 end
